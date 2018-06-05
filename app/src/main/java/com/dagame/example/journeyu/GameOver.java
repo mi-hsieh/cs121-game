694ccbd57800cb99711b1a80f4c3c1c7670a208e
Sheng Xu <xusheng228@gmail.com>
2018-06-05 13:55:55 -0700

    GameOver Music

--- app/src/main/java/com/dagame/example/journeyu/GameOver.java
+++ app/src/main/java/com/dagame/example/journeyu/GameOver.java
@@ -5,15 +5,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
+import android.media.MediaPlayer;
+
 
 public class GameOver extends AppCompatActivity {
+
+    //add gameover music
+    MediaPlayer sndgo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over);
+
+        sndgo = MediaPlayer.create(GameOver.this, R.raw.gameover);
+        sndgo.setLooping(true);
+        sndgo.start();
+
     }
 
+    @Override
+    protected void onPause(){
+        super.onPause();
+        sndgo.release();
+        finish();
+
+    }
     /* Called when the user taps the Send button */
     public void onReplayClicked(View view) {
         Intent intent = new Intent(this, MainActivity.class);
